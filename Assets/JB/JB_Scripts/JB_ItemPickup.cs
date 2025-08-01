f59d4ef709b856567ad2d34bc12de9fcb425bb06
JoaoBosco4333 <joaoboscogamer04@gmail.com>
2025-08-01 11:00:35 -0300

    Mensagem para de Use P
    [ADD]
    adicionei uma mensagem para avisar para Usar P para coletar o item

--- Assets/JB/JB_Scripts/JB_ItemPickup.cs
+++ Assets/JB/JB_Scripts/JB_ItemPickup.cs
@@ -3,13 +3,32 @@ using UnityEngine;
 public class JB_ItemPickup : MonoBehaviour
 {
     public Sprite itemSprite; // O sprite que aparecerá no slot
+    public JB_ColetarMensagem coletarMensagem; // Arraste o script do Canvas aqui
 
     public void Collect()
     {
         JB_Inventory inv = FindObjectOfType<JB_Inventory>();
         if (inv != null && inv.AddItem(itemSprite))
         {
+            if (coletarMensagem != null)
+                coletarMensagem.HideMessage();
             Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && coletarMensagem != null)
+        {
+            coletarMensagem.ShowMessage();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && coletarMensagem != null)
+        {
+            coletarMensagem.HideMessage();
         }
     }
 
@@ -17,13 +36,7 @@ public class JB_ItemPickup : MonoBehaviour
     {
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.P))
         {
-            JB_Inventory inv = FindObjectOfType<JB_Inventory>();
-            if (inv != null && inv.AddItem(itemSprite))
-            {
-                Destroy(gameObject); // Remove o item do mundo
-            }
+            Collect();
         }
     }
-
-
 }
