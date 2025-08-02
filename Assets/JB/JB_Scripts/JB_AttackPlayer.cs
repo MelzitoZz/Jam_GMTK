a8a75be4ff38c5c5344cda385e8d99f52d3f4fb9
MelzitoZz <168978954+MelzitoZz@users.noreply.github.com>
2025-08-01 22:06:58 -0300

    ADD ataque do personagem

--- Assets/JB/JB_Scripts/JB_AttackPlayer.cs
+++ Assets/JB/JB_Scripts/JB_AttackPlayer.cs
@@ -7,26 +7,39 @@ public class JB_AttackPlayer : MonoBehaviour
     public LayerMask destruivelLayer;
     public int damage = 1;
     public KeyCode attackKey = KeyCode.Space;
+    public Animator animator;
+    public float attackDelay = 0.3f;
+    public float attackCooldown = 0.5f;
+
+    private bool canAttack = true;
 
     void Update()
     {
-        if (Input.GetKeyDown(attackKey))
+        if (Input.GetKeyDown(attackKey) && canAttack)
         {
-            Attack();
+            if (animator != null)
+                animator.SetTrigger("Attack");
+
+            canAttack = false;
+            Invoke(nameof(DoAttack), attackDelay);
+            Invoke(nameof(ResetAttack), attackCooldown);
         }
     }
 
-    void Attack()
+    void DoAttack()
     {
         Collider2D[] hitObjects = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, destruivelLayer);
         foreach (Collider2D obj in hitObjects)
         {
             JB_ObjetoDestruivel destruivel = obj.GetComponent<JB_ObjetoDestruivel>();
             if (destruivel != null)
-            {
                 destruivel.TakeHit(damage);
-            }
         }
+    }
+
+    void ResetAttack()
+    {
+        canAttack = true;
     }
 
     void OnDrawGizmosSelected()
